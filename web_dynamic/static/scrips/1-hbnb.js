216b3839dae5340e82efff077debda53d78f964a
Vozudo <jploperatobon@gmail.com>
2023-01-04 22:18:49 -0500

    Task 1: update

---
+++ web_dynamic/static/scrips/1-hbnb.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  const dictamenity = {};
+  $('input[type="Checkbox"]'.click(function () {
+    if ($(this).is(":checked")) {
+      dictamenity[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else {
+      delete dictamenity[$(this).attr('data-name')];
+    }
+    const name_amenity = Object.keys(dictamenity);
+    $('.amenities h4').text(names.sort().join(', '));
+  }));
+});

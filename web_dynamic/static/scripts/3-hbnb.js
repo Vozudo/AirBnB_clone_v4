6a0f4e27641c23c615fdf7ee166f53ff0129a767
raquintero1974 <4638@holbertonstudents.com>
2023-01-06 10:55:36 -0500

    Task 3 update

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -19,4 +19,31 @@ $(document).ready(function () {
       }
     });
   })
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:5001/api/v1/places_search',
+    data: JSON.stringify({ amenities: Object.values(dictamenity) }),
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      $('SECTION.places').empty();
+      $('SECTION.places').append(data.map(place => {
+        return `<article>
+                  <div class="title_box">
+                    <h2>${place.name}</h2>
+                    <div class="price_by_night">$${place.price_by_night}</div>
+                  </div>
+                  <div class="information">
+                    <div class="max_guest">${place.max_guest} Guests</div>
+                    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                  </div>
+                  <div class="description">
+                    ${place.description}
+                  </div>
+                </article>`
+      }));
+    }
+  });
 });
